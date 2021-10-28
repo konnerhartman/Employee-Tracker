cd52ef50bc109739e7294c240c9b67741d586a20
Konner Hartman <konner.hartman@yahoo.com>
2021-10-28 18:22:05 -0500

    added three functions for showing tables.

--- server.js
+++ server.js
@@ -28,14 +28,15 @@ const startPrompt = () => {
                 'Add a department.',
                 'Add a role.',
                 'Add an employee.',
-                'Update an employee role.'
+                'Update an employee role.',
+                'EXIT'
             ]
         }
     ])
     .then(answer => {
         switch (answer.selected) {
             case 'View all departments.':
-                viewAllDeps();
+                viewAllDepartments();
             break;
 
             case 'View all roles.':
@@ -43,11 +44,11 @@ const startPrompt = () => {
             break;
 
             case 'View all employees.':
-                viewAllEmps();
+                viewAllEmployees();
             break;
 
             case 'Add a department.':
-                addDep();
+                addDepartment();
             break;
 
             case 'Add a role.':
@@ -55,12 +56,93 @@ const startPrompt = () => {
             break;
 
             case 'Add an employee.':
-                addEmp();
+                addEmployee();
             break;
 
             case 'Update an employee role.':
                 updateRole();
             break;
+
+            case 'EXIT':
+                exitApp();
+            break;
+
+            default:
+            break;
         };
     });
+};
+
+viewAllDepartments = () => {
+    connection.query(
+        `SELECT employee.first_name AS FIRST, employee.last_name AS LAST, department.name AS DEPARTMENT FROM employee 
+        JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id 
+        ORDER BY employee.id;`,
+        (err, res) => {
+            if (err) throw err
+            console.table(res)
+            startPrompt()
+        })
+};
+
+viewAllRoles = () => {
+    connection.query(
+        `SELECT employee.first_name AS FIRST, employee.last_name AS LAST, role.title AS TITLE FROM employee
+        JOIN role ON employee.role_id = role.id;`,
+        (err, res) => {
+            if (err) throw err
+            console.table(res)
+            startPrompt()
+        }
+    )
+};
+
+viewAllEmployees = () => {
+    connection.query(
+        `SELECT employee.first_name AS FIRST, employee.last_name AS LAST, role.title AS TITLE, role.salary AS SALARY, department.name AS DEPARTMENT, CONCAT(e.first_name, " ", e.last_name) AS MANAGER FROM employee 
+        INNER JOIN role on role.id = employee.role_id 
+        INNER JOIN department on department.id = role.department_id 
+        LEFT JOIN employee e on employee.manager_id = e.id;`,
+        (err, res) => {
+            if (err) throw err
+            console.table(res)
+            startPrompt()
+        }
+    )
+};
+
+addDepartment = () => {
+    connection.query('', (err, res) => {
+        if (err) throw err
+        console.table(res)
+        startPrompt()
+    })
+};
+
+addRole = () => {
+    connection.query('', (err, res) => {
+        if (err) throw err
+        console.table(res)
+        startPrompt()
+    })
+};
+
+addEmployee = () => {
+    connection.query('', (err, res) => {
+        if (err) throw err
+        console.table(res)
+        startPrompt()
+    })
+};
+
+updateRole = () => {
+    connection.query('', (err, res) => {
+        if (err) throw err
+        console.table(res)
+        startPrompt()
+    })
+};
+
+exitApp = () => {
+    connection.end();
 };
